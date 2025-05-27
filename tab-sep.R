15dfa1546d37e60e5ea832e987952293a5af28c0
Ondrej Such <ondrej.such@gmail.com>
2025-05-27 09:24:57 +0200

    Added file

---
+++ tab-sep.R
@@ -0,0 +1,20 @@
+library(tidyr)
+library(dplyr)
+library(xtable)
+
+df <- read.csv("data/separation.csv")
+
+df1 <- df |> group_by(dataset, status, K) |> summarize(count = n() ) |>
+  mutate(pct = count / (10 * K * (K-1))) |>
+  pivot_wider(id_cols = c("dataset", "K"), 
+              names_from = "status", 
+              values_from = "pct", 
+              values_fill = 0) |>
+  mutate(separable = optimal) |>
+  select(dataset, K)
+
+sink("tab-sep.tex")
+print(xtable(df1, label = "tab:sep", digits = 2,
+             caption = "The benchmark datasets with indication of the number of classes $K$ ",
+),  include.rownames = FALSE)
+sink()
